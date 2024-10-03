050ee5bdea60d8c643da75f1623390da6db875e8
Arath Villanueva <73966440+ArathBV@users.noreply.github.com>
2024-10-02 20:58:09 -0700

    Create lab2sub.py

---
+++ Lab2/lab2sub.py
@@ -0,0 +1,32 @@
+import rclpy
+import math
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class squared(Node):
+
+    def __init__(self):
+        super().__init__('squared')
+        
+        self.subscription = self.create_subscription(
+            String,
+            'numbers'
+            self.listener_callback,
+            10)
+        self.subscription
+    
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%d"' % (msg.data ** 2))
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    sub = squared()
+    rclpy.spin(sub)
+
+    sub.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
