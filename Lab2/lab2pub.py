01bc3617d19d498ab66952b9fefd32a4175ee7fd
Arath Villanueva <73966440+ArathBV@users.noreply.github.com>
2024-10-03 17:11:39 -0700

    Update lab2pub.py

--- Lab2/lab2pub.py
+++ Lab2/lab2pub.py
@@ -1,24 +1,24 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from std_msgs.msg import Int32
 
 class talker(Node):
 
     def __init__(self):
         super().__init__('talker')
-        
-        self.publisher_ = self.create_publisher(String, 'numbers', 10)
+
+        self.publisher_ = self.create_publisher(Int32, 'numbers', 10)
 
         timer_period = 0.5
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-    
+
     def timer_callback(self):
-        msg = String()
-        msg.data = '%d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        msg = Int32()
+        msg.data = self.i
+        self.publisher_.publish(msg)    
+        self.get_logger().info('Publishing: "%d"' % msg.data)
         self.i += 1
 
 def main(args=None):
@@ -32,3 +32,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+    
